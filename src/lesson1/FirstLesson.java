2da662229305fffba6dd70dab539cf9b9f3a549f
User <ifmo@gmail.com>
2020-09-25 21:43:55 +0300

    lesson 2

--- src/lesson1/FirstLesson.java
+++ src/lesson1/FirstLesson.java
@@ -11,7 +11,7 @@ public class FirstLesson {
 
     // точка входа в программу
     // метод main  (psvm + Enter)
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // консольный вывод // sout + Enter
         System.out.println("FirstLesson");
 
