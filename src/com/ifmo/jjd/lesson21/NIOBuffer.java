8e8b10d9da60047aaa2dbe9ad8740b04e5f8905f
nikita <adrbrug@gmail.com>
2020-11-13 22:39:34 +0300

    lesson21++

--- src/com/ifmo/jjd/lesson21/NIOBuffer.java
+++ src/com/ifmo/jjd/lesson21/NIOBuffer.java
@@ -1,16 +1,64 @@
 package com.ifmo.jjd.lesson21;
 
 import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 public class NIOBuffer {
     public static void main(String[] args) {
 
+        // пакет nio (new io / non-blocking io) предназначен для работы с вводом и выводом
+        // * неблокирующий
+        // * асинхронный
+        // * буфер-ориентированный
+
+
+
+        // Path, Files
+        // набор классов Channel, набор классов Buffer, селекторы
+
+
+        // Каналы: программа буфер ======= файл
+        // можно использовать и для записи и для чтения
+        // события в канале могут происходить асинхронно
+        // каналы всегда читают из буфера и пишут в буфер
+        // FileChannel - для работы с файлами
+        // SocketChannel - клиентский сокет, используется для подключения к серверу
+        // и передачи данных по TCP протоколу
+        // DatagramChannel - клиентский сокет, используется для подключения к серверу
+        // и передачи данных по UDP протоколу
+        // ServerSocketChannel - используется на сервере
+
+        // Буферы:
+        // ByteBuffer
+        // CharBuffer
+        // DoubleBuffer / FloatBuffer
+        // ShortBuffer / IntBuffer / LongBuffer
+
+        // свойства буферов:
+        // capacity - емкость (задается при создании буфера и не меняется)
+        // position - текущая позиция в буфере (изначально равна 0)
+        // limit - указатель, до какого значения можно читать / писать данные
+        // (изначально равен capacity)
+
+        // assert используются только при разработке,
+        // чтобы они работали их нужно включить:
+        // Edit Configuration -> VM options: -ea
+
+
         ByteBuffer buffer = ByteBuffer.allocate(16);
+        System.out.println(buffer.position()); // возвращает позицию
+        System.out.println(buffer.capacity()); // возвращает емкость
+        System.out.println(buffer.limit()); // возвращает значение limit
+        System.out.println(buffer.remaining()); // возвращает разницу между position и limit
         assert buffer.position() == 0;
         assert buffer.capacity() == 16;
         assert buffer.limit() == 16;
         assert buffer.remaining() == 16; // разница между position и limit
 
+        // put - запись в буфер
         // Увеличивает позицию на 4.
         buffer.putInt(100);
 
@@ -23,7 +71,8 @@ public class NIOBuffer {
         assert buffer.position() == 12;
         assert buffer.remaining() == 4;
 
-        // Устанавливает лимит на место позиции, сбрасывает позицию в 0 (для чтения из буфера)
+        // Подготовка буфера для чтения:
+        // устанавливает лимит на место позиции, сбрасывает позицию в 0 (для чтения из буфера)
         buffer.flip();
 
         assert buffer.position() == 0;
@@ -61,6 +110,13 @@ public class NIOBuffer {
         assert buffer.capacity() == 16;
         assert buffer.limit() == 16;
         assert buffer.remaining() == 16;
+
+        // для установки значений вручную buffer.position(2);
+
+        // копирует все непрочитанные данные в начало буфера,
+        // установит limit равной емкости буфера
+        // Буфер готов для записи после непрочитанных данных
+        // buffer.compact();
 
     }
 }
